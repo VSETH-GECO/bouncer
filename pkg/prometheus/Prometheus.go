9af422f0f9d962f404ace6a305ac04bae146f86c
Florian Bütler <florian.buetler@proton.me>
2025-04-18 18:07:25 +0200

    allow reading metrics from any host

--- pkg/prometheus/Prometheus.go
+++ pkg/prometheus/Prometheus.go
@@ -1,18 +1,19 @@
 package prometheus
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/VSETH-GECO/bouncer/pkg/database"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
-	"net/http"
-	"strconv"
 )
 
 func StartServing(port int, handler *database.Handler) {
 	go database.StartUpdating(handler)
 
 	http.Handle("/metrics", promhttp.Handler())
-	err := http.ListenAndServe("127.0.0.1:"+strconv.Itoa(port), nil)
+	err := http.ListenAndServe("0.0.0.0:"+strconv.Itoa(port), nil)
 	if err != nil {
 		log.WithError(err).Fatal("Couldn't start prometheus handler")
 	}
